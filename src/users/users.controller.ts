89624e27b0511f5d8024f4ed16e328d3c03df5fb
Leandro <atiladevelop@gmail.com>
2024-02-15 01:59:10 -0300

    refactoring custom interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,14 @@ import {
   Query,
   Delete,
   NotFoundException,
+  UseInterceptors,
+  // ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -22,6 +26,7 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.userService.findOne(parseInt(id));
